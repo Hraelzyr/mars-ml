8c3b8503a80422ef9ec0ba47882057a56143e2fd
artemisia <arvind.srinivasn@gmail.com>
2023-05-18 15:28:12 +0530

    This SLURM option works afaik

--- simple_multinode/slurm-file.sh
+++ simple_multinode/slurm-file.sh
@@ -3,29 +3,29 @@
 #SBATCH --nodes=2
 #SBATCH --gpus=4
 #SBATCH --job-name=multinode-ml
-#SBATCH --cpus-per-task=4
+##SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-task=2
-##SBATCH --time=0-00:10:00
-##SBATCH --partition=debug
-##SBATCH --gres=gpu:2
+#SBATCH --time=0-00:10:00
+#SBATCH --partition=debug
+#SBATCH --gres=gpu:2
 #SBATCH --error=file.err
 #SBATCH --output=file.out
 
 ml cuda
 
 #nvidia-smi
-cp -r ../data /dev/shm/simple-$J
-nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
-nodes_array=($nodes)
-head_node=${nodes_array[0]}
-head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+#cp -r ../data /dev/shm/
 
-echo Node IP: $head_node_ip
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+export OMP_NUM_THREADS=32
+echo Node IP: $master_addr
 srun torchrun \
 --nnodes 2 \
---nproc_per_node 2 \
+--nproc_per_node gpu \
 --rdzv_id $RANDOM \
 --rdzv_backend c10d \
---rdzv_endpoint $head_node_ip:29500 \
+--rdzv_endpoint $master_addr:29500 \
 main.py
-rm -r /dev/shm/simple-$J
+
+#rm -r /dev/shm/data
