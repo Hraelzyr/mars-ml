44f6efe6e1cf3303459d214af48acc92e632bea0
artemisia <arvind.srinivasn@gmail.com>
2023-05-18 11:58:44 +0530

    More model changes, and should be multinode now

--- simple_multinode/slurm-file.sh
+++ simple_multinode/slurm-file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --nodes=1
-#SBATCH --job-name=test
-##SBATCH --cpus-per-task=cpu
+#SBATCH --nodes=4
+#SBATCH --job-name=multinode-ml
+#SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-task=2
 ##SBATCH --time=0-00:10:00
 ##SBATCH --partition=debug
@@ -14,5 +14,17 @@ ml cuda
 
 #nvidia-smi
 cp -r ../data /dev/shm/simple-$J
-srun torchrun --standalone --nproc_per_node=gpu main.py
+nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
+nodes_array=($nodes)
+head_node=${nodes_array[0]}
+head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
+
+echo Node IP: $head_node_ip
+srun torchrun \
+--nnodes 4 \
+--nproc_per_node 2 \
+--rdzv_id $RANDOM \
+--rdzv_backend c10d \
+--rdzv_endpoint $head_node_ip:29500 \
+main.py
 rm -r /dev/shm/simple-$J
